fb39a25563b7297a24fab4389b4fb7c7a2c108fa
Adsicmes <1483492332@qq.com>
2021-10-02 18:07:28 +0800

    略作修改

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tools.recent.std import main as re_std
+import os
 
 
-def main():
+def recent():
     try:
         re_std.main()
     except Exception as e:
@@ -9,5 +10,22 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
-    input('按下回车退出')
+    text = """1.`+`recent成绩图生成"""
+    while True:
+        print(r""" _____   _       _____   _____   _____   _____   _____   _       _____  
+/  _  \ | |     /  _  \ /  ___| |_   _| /  _  \ /  _  \ | |     /  ___/ 
+| | | | | |     | | | | | |       | |   | | | | | | | | | |     | |___  
+| | | | | |     | | | | | |       | |   | | | | | | | | | |     \___  \ 
+| |_| | | |___  | |_| | | |___    | |   | |_| | | |_| | | |___   ___| | 
+\_____/ |_____| \_____/ \_____|   |_|   \_____/ \_____/ |_____| /_____/ 
+                                                    -----by Adsicmes""")
+        print("""\n\n全本地功能标`+`，涉及到网络的功能标`-`""")
+        print("""目前已有功能:\n""")
+        print(text)
+        sec = input('\n请输入要使用的功能:')
+        if sec == 1:
+            recent()
+        input('按下回车返回主界面')
+        os.system('clear')
+
+
